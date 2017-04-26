691a6e4608920bfe80a37249d7cc6060a5be8cfc
Janek Brandt <brandt.janek@gmail.com>
2017-04-25 20:17:03 -0700

    add make admin button to navbar which toggles edit story button and add story button

--- app/components/admin-test.js
+++ app/components/admin-test.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  admin: Ember.inject.service('admin'),
-  actions: {
-    makeAdmin() {
-      this.get('admin').makeAdmin();
-    }
-  }
+  admin: Ember.inject.service('admin')
 });
---
+++ app/components/make-admin.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  admin: Ember.inject.service('admin'),
+  actions: {
+    makeAdmin() {
+      this.get('admin').makeAdmin();
+    },
+    removeAdmin() {
+      this.get('admin').removeAdmin();
+    }
+  }
+});
